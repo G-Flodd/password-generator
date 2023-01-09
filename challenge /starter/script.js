1ff009b66d3a5661b5d20b10707fd498534210a9
G-Flodd <georgedodd@hotmail.co.uk>
2023-01-09 19:41:39 +0000

    Additional java added, array can now be randomised

--- challenge /starter/script.js
+++ challenge /starter/script.js
@@ -88,12 +88,13 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
- var characterOptions = [];
+var characterOptions = [];
+var chosenLength;
 
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-  var chosenLength = prompt("Please choose a password length between 10 and 64 ");
+  chosenLength = prompt("Please choose a password length between 10 and 64 ");
   if (chosenLength > 64 || chosenLength < 10) {
     chosenLength = prompt("Invalid selection, please choose a password length between 10 and 64 ");
 
@@ -101,36 +102,64 @@ function getPasswordOptions() {
 
 
   var lowerCase = confirm("Would you like lower case characters");
-   if(lowerCase === true ) {
-    characterOptions.push(lowerCasedCharacters);
-      console.log(characterOptions);
-   }
+  if (lowerCase === true) {
+    characterOptions = characterOptions.concat(lowerCasedCharacters);
+    console.log(characterOptions);
+  }
 
 
   var upperCase = confirm("Would you like upper case characters");
-  if(upperCase === true ) {
-    characterOptions.concat(upperCasedCharacters);
-      console.log(characterOptions);
-   }
+  if (upperCase === true) {
+    characterOptions = characterOptions.concat(upperCasedCharacters);
+    console.log(characterOptions);
+  }
 
 
   var numeric = confirm("Would you like numerical characters");
-  if(numeric === true ) {
-    characterOptions.concat(numericCharacters);
-      console.log(characterOptions);
-   }
+  if (numeric === true) {
+    characterOptions = characterOptions.concat(numericCharacters);
+    console.log(characterOptions);
+  }
 
 
   var special = confirm("Would you like special characters");
-  if(special === true ) {
-    characterOptions.concat(specialCharacters);
-      console.log(characterOptions);
-   }
+  if (special === true) {
+    characterOptions = characterOptions.concat(specialCharacters);
+    console.log(characterOptions);
+  }
 
 }
 
+
+
+// Array shuffle aka lets shuffle 
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+
 // Function for getting a random element from an array
 function getRandom(arr) {
+  arrayShuffled = shuffle(arr);
+  console.log(arrayShuffled);
+
+  for(let i=0; i < chosenLength; i++)  
 
 }
 
@@ -138,8 +167,11 @@ function getRandom(arr) {
 function generatePassword() {
   // if the user has asked for lowercase, then you need too 
   getPasswordOptions();
+  getRandom(characterOptions);
 
 }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
